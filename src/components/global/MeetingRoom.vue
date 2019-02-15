b1cc27a181f84fb8d53c6b30dd5739869ba7e764
杨林 <yanglin001@zhongan.com>
2019-02-15 17:24:18 +0800

    commit

---
+++ src/components/global/MeetingRoom.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    我是一个全局组件
+  </div>
+</template>
+<script>
+export default {
+  name: 'meetingRoom'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MeetingRoom></MeetingRoom>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { getUserInfo } from '@/api/test'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  created () {
+    console.log(process.env.BASE_URL)
+    this.getTest();
+  },
+  methods:{
+    async getTest(){
+      let res = await getUserInfo();
+      console.log(res)
+    }
+  }
+}
+</script>
